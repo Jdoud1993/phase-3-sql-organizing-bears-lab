bcb1b55e97860b2de186e78dfbaf16af2aa90431
Joshua Doud <bigjj3393@yahoo.com>
2023-02-22 19:05:41 -0800

    commit tree ec5ce561ae168a13fb2ceba4a4b6aecef824484d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (4, 'M', 'brown', 'friendly', true);
